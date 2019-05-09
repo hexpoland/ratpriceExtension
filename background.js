62ee169e323fbbc1aed9f8a0cbb1d3172b2358de
h3x <h3x@MacBook-Air-h3x.local>
2019-05-09 23:03:15 +0200

    userId and Avatar in parts on firebase

--- background.js
+++ background.js
@@ -1,47 +1,47 @@
 var contextMenuItem = {
-  id: "CennikRational",
-  title: "CennikRational",
-  contexts: ["selection"]
-};
-chrome.contextMenus.create(contextMenuItem);
+  id: 'CennikRational',
+  title: 'CennikRational',
+  contexts: ['selection']
+}
+chrome.contextMenus.create(contextMenuItem)
 
-chrome.runtime.onInstalled.addListener(function() {
-  console.log("Zainstalowano RATIONAL PARTS MANAGER");
-});
+chrome.runtime.onInstalled.addListener(function () {
+  console.log('Zainstalowano RATIONAL PARTS MANAGER')
+})
 
-chrome.tabs.onActiveChanged.addListener(() => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
-    var url = tabs[0].url;
-    console.log(url);
-  });
-});
+// chrome.tabs.onActiveChanged.addListener(() => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+//     var url = tabs[0].url
+//     console.log(url)
+//   })
+// })
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log(
     sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
-});
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension'
+  )
+  if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' })
+})
 // FIREBASE
 var config = {
-  apiKey: "AIzaSyAyUr_KLRq3_EpC7iZ6-5efhAITfKgE4Rs",
-  authDomain: "partsextension.firebaseapp.com",
-  databaseURL: "https://partsextension.firebaseio.com",
-  projectId: "partsextension",
-  storageBucket: "partsextension.appspot.com",
-  messagingSenderId: "750984928739"
-};
-var partsListFirebase = [];
+  apiKey: 'AIzaSyAyUr_KLRq3_EpC7iZ6-5efhAITfKgE4Rs',
+  authDomain: 'partsextension.firebaseapp.com',
+  databaseURL: 'https://partsextension.firebaseio.com',
+  projectId: 'partsextension',
+  storageBucket: 'partsextension.appspot.com',
+  messagingSenderId: '750984928739'
+}
+var partsListFirebase = []
 
-const app = firebase.initializeApp(config);
-const db = app.firestore();
+const app = firebase.initializeApp(config)
+const db = app.firestore()
 
 chrome.runtime.onMessage.addListener((msg, sender, callback) => {
-  console.log(msg);
-  if (msg.type === "firebase") {
-    db.collection("parts")
+  console.log(msg)
+  if (msg.type === 'firebase') {
+    db.collection('parts')
       .doc(msg.numer)
       .set({
         // numer: msg.numer,
@@ -50,91 +50,100 @@ chrome.runtime.onMessage.addListener((msg, sender, callback) => {
         nazwa: msg.nazwa,
         cena: msg.cena
       })
-      .then(function() {
-        console.log("Document written to firebase");
+      .then(function () {
+        console.log('Document written to firebase')
       })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      });
-  } else if (msg.type === "loginFirebase") {
+      .catch(function (error) {
+        console.error('Error adding document: ', error)
+      })
+  } else if (msg.type === 'loginFirebase') {
     let user = loginToFirebase().then(e => {
       getFromFirebase().then(() => {
-        console.log(partsListFirebase);
-      });
-      console.log("zalogowany", e);
-      chrome.runtime.sendMessage({ type: "logedIn", user: e });
-    });
-  } else if (msg.type === "logoutFirebase") {
-    logoutFromFirebase();
+        console.log(partsListFirebase)
+      })
+      console.log('zalogowany', e)
+      chrome.runtime.sendMessage({ type: 'logedIn', user: e })
+    })
+  } else if (msg.type === 'logoutFirebase') {
+    logoutFromFirebase()
   }
-});
-db.collection("parts").onSnapshot(() => {
+})
+
+db.collection('parts').onSnapshot(() => {
   // console.log('Dodano nowy element info form watcher')
-  db.collection("parts")
+  db.collection('parts')
     .get()
     .then(e => {
       // console.log(e.docs[e.size - 1].id)
-      db.collection("parts")
+      db.collection('parts')
         .doc(e.docs[e.size - 1].id)
         .get()
         .then(r => {
-          console.log(r.data());
-        });
-    });
-});
+          console.log(r.data())
+        })
+    })
+})
 
-async function loginToFirebase() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope("profile");
-  provider.addScope("email");
-  function loginToFirebase() {}
-  let uFirebase;
+async function loginToFirebase () {
+  var provider = new firebase.auth.GoogleAuthProvider()
+  provider.addScope('profile')
+  provider.addScope('email')
+  let uFirebase
   let u = await firebase
     .auth()
     .signInWithPopup(provider)
     .then(e => {
-      localStorage.setItem("userEmail", e.user.email);
-      localStorage.setItem("userId", e.user.uid);
-      localStorage.setItem("userAvatar", e.user.photoURL);
-      localStorage.setItem("live", true);
-      uFirebase = e.user;
-      return e.user;
-    }); // Opens a popup window and returns a promise to handle errors.
-  return uFirebase;
+      localStorage.setItem('userEmail', e.user.email)
+      localStorage.setItem('userId', e.user.uid)
+      localStorage.setItem('userAvatar', e.user.photoURL)
+      localStorage.setItem('live', true)
+      uFirebase = e.user
+      return e.user
+    })
+    .catch(error => {
+      alert('Ups cos poszło nie tak')
+      console.log(error)
+    }) // Opens a popup window and returns a promise to handle errors.
+  return uFirebase
 }
-function logoutFromFirebase() {
+function logoutFromFirebase () {
   firebase
     .auth()
     .signOut()
-    .then(function() {
-      chrome.runtime.sendMessage({ type: "logedOut" });
-      console.log("LOGOUT!!!");
-      localStorage.setItem("live", false);
-      localStorage.setItem("userId", "");
-      localStorage.setItem("userAvatar", "");
+    .then(function () {
+      chrome.runtime.sendMessage({ type: 'logedOut' })
+      console.log('LOGOUT!!!')
+      localStorage.setItem('live', false)
+      localStorage.setItem('userId', '')
+      localStorage.setItem('userAvatar', '')
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // An error happened.
-    });
+    })
 }
-async function getFromFirebase() {
-  partsListFirebase = []; //clear to dont repeat records
-  db.collection("parts")
+async function getFromFirebase () {
+  partsListFirebase = [] // clear to dont repeat records
+  db.collection('parts')
     .get()
     .then(e => {
       e.docs.forEach((el, val, index) => {
-        db.collection("parts")
+        db.collection('parts')
           .doc(el.id)
           .get()
           .then(r => {
             let part = {
               id: el.id,
+              user: r.data().user,
+              userAvatar: r.data().userAvatar,
               nazwa: r.data().nazwa,
               cena: r.data().cena
-            };
+            }
             // console.log(part)
-            partsListFirebase.push(part);
-          });
-      });
-    });
+            partsListFirebase.push(part)
+          })
+      })
+    })
+    .then(() => {
+      console.log(partsListFirebase)
+    })
 }
